2fb3355db99b935775e6069e1480f145da95297c
Joel <joel960801@gmail.com>
2022-09-10 00:09:45 +0200

    Added meals items and displayed them

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,38 +1,49 @@
 import React from "react";
+import Card from "../UI/Card";
 import style from "./AvailableMeals.module.css";
+import MealItem from "./MealItem/MealItem";
 
 const MEALS = [
   {
     id: "m1",
     name: "Sushi",
     description: "Finest fish and veggies",
-    price: 22.99,
+    price: 89.99,
   },
   {
     id: "m2",
     name: "Schnitzel",
     description: "A german specialty!",
-    price: 16.5,
+    price: 80.5,
   },
   {
     id: "m3",
     name: "Barbecue Burger",
     description: "American, raw, meaty",
-    price: 12.99,
+    price: 120.99,
   },
   {
     id: "m4",
     name: "Green Bowl",
     description: "Healthy...and green...",
-    price: 18.99,
+    price: 135.99,
   },
 ];
 
 const AvailableMeals = () => {
-  const meals = MEALS.map((meal) => <li>{meal.name}</li>);
+  const meals = MEALS.map((meal) => (
+    <MealItem
+      key={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
+  ));
   return (
     <section className={style.meals}>
-      <ul>{meals}</ul>
+      <Card>
+        <ul>{meals}</ul>
+      </Card>
     </section>
   );
 };
